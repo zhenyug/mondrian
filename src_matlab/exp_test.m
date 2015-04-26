fa903a603689668b84ea95faa1072de0a642d1c4
zhenyug <zhenyug@ece.ubc.ca>
2015-04-26 02:57:41 -0700

    update

--- src_matlab/exp_test.m
+++ src_matlab/exp_test.m
@@ -1,20 +1,23 @@
-function pred = exp_test(image_path)
-run('../external/vlfeat-0.9.20/toolbox/vl_setup.m')
+function result = exp_test(image_path)
+run('~/mondrian/external/vlfeat-0.9.20/toolbox/vl_setup.m')
 
 food_classify = load('food_classify.mat');
+% load form workspace in python
+
 im = imread(image_path);
 scale_factor = 480/max(size(im));
 im = imresize(im, scale_factor, 'bilinear');
 descrs = food_classify.encodeFunc(food_classify.encoder,im);
 descrs = vl_homkermap(descrs,1,'kchi2') ;
 scores = food_classify.w' * descrs + food_classify.b;
-pred = scores > 0;
-pred_label = {'food', 'non-food'};
-if pred == 1
-    fprintf('this image is %s\n with a score: %.2f\n', pred_label{1}, scores);
+if scores > 0
+  pred = 1;
 else
-    fprintf('this image is %s\n with a score: %.2f\n', pred_label{2}, scores);
+  pred = 2;
 end
-imshow(im);
 
+pred_label = {'food', 'non-food'};
+result.pred = pred;
+result.score = scores;
+result.label = pred_label(pred);
 
